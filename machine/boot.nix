56c4110eac3bda0e818d5ad2bf4e386a1fdafeb8
Espen Trydal <espen@trydal.io>
2022-07-04 00:11:17 +0200

    initial commit

---
+++ machine/boot.nix
@@ -0,0 +1,33 @@
+{
+  # Bootloader.
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  #boot.cleanTmpDir = true;
+  #boot.tmpOnTmpfs = true;
+
+  boot.loader = {
+     efi = {
+       canTouchEfiVariables = true;
+       efiSysMountPoint = "/boot/efi";
+     };
+     grub = {
+       devices =[ "nodev" ];
+       efiSupport = true;
+       enable = true;
+       extraEntries = ''
+         menuentry "Windows" {
+	  insmod part_gpt
+	  insmod fat
+	  insmod search_fs_uuid
+	  insmod chain
+	  search --fs-uuid --set=root 72F8-2745
+	  chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+      version = 2;
+    };
+  };
+
+}
